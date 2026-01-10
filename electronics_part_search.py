c6a3c818d84d183cb0d0cd1817dff1823b2a6385
aarjac <56480900+aarjac@users.noreply.github.com>
2026-01-10 18:18:47 -0500

    Add current_parts_list tracker

--- electronics_part_search.py
+++ electronics_part_search.py
@@ -1,18 +1,72 @@
 from pathlib import Path
 
+class PartsList():
+    
+    def __init__(self, parts):
+        self.parts = []
+
+    def __str__(self):
+        pass
+
+class Part():
+    
+    def __init__(self, part_number='', part_type='', manufacturer=''):
+        self.part_number = ''
+        self.part_type = ''
+        self.manufacturer = ''
+
+    def get_part_number(self):
+        checking_part_number = True
+        verify_part_number = ''
+        self.part_number = input('Enter Part Number: ')
+        while checking_part_number == True:
+            verify_part_number = input(f'Is this the correct part number? {self.part_number} (Y/N): ')
+            if verify_part_number.upper() == 'Y':
+                print(f'You have enetered the part number {self.part_number}')
+                break
+            else:
+                self.part_number = input('Enter Part Number: ')
+                continue
+
+    def get_part_type(self):
+        checking_part_type = True
+        verify_part_type = ''
+        self.part_type = input('Enter Part Type: ')
+        self.part_type = self.part_type.split(' ')
+        print(self.part_type)
+        self.part_type = '_'.join(self.part_type)
+        while checking_part_type == True:
+            verify_part_type = input(f'Is this the correct part type? {self.part_type} (Y/N): ')
+            if verify_part_type.upper() == 'Y':
+                print(f'You have enetered the part type {self.part_type}')
+                break
+            else:
+                self.part_type = input('Enter Part Type: ')
+                continue
+
+            
+
 def main():
 
-    part_number = input('Enter Part Number: ')
-    part_type = input('Enter Part Type: ')
-    part_manufacturer = input('Enter Manufacturer')
+    parts_file = open('current_parts_list.txt', 'r')
+    current_parts_list = parts_file.read()
+    current_parts_list = current_parts_list.split()
+    print(current_parts_list)
+    
+    new_part = Part()
+    new_part.get_part_number()
+    print(new_part.part_number)
+    new_part.get_part_type()
+    print(new_part.part_type)
+    
 
 
 
-    new_path = "C:\\Users\\aaron\\OneDrive\\Documents\\VScode\\Python\\python_programs\\sandbox_testing" + "\\" + part_type + "\\" + part_number
+    #new_path = "C:\\Users\\aaron\\OneDrive\\Documents\\VScode\\Python\\python_programs\\sandbox_testing" + "\\" + part_type + "\\" + part_number
 
-    print(new_path)
+    #print(new_path)
 
-    Path(new_path).mkdir(parents=True, exist_ok=True)
+    #Path(new_path).mkdir(parents=True, exist_ok=True)
 
 if __name__ == '__main__':
     main()
--- readme.txt
+++ readme.txt
@@ -1,3 +1,15 @@
 Electornics Part Search
 #######################
 
+Phase I function:
+-add parts to database
+    -part manufacturer, part number, description, part type
+-create/manage file system for parts
+-log added parts to a list 
+-view current parts list
+
+Phase II functionality:
+-download datasheet and place into part folder
+
+Phase II functionality: 
+-download part footprint, SPICE model, 3D model
